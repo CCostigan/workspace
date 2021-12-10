b6830494173fcfc37400ab97a95167f2f56fd94f
CCostigan <ccc004@yahoo.com>
2021-12-10 14:06:07 +0000

    Recent changes

--- Python_01/Hello.py
+++ Python_01/Hello.py
@@ -1,4 +1,13 @@
 
+import os
+
+class Hello():
+    def __init__(self, filename):
+        print("TESTING 1...2...3...")
+                
+
 if __name__=='__main__':
-    print "Hello Mac (from Python)"
+    print("Hello Mac (from Python)")
+    print("File location: {}".format( __file__ ))
+    Hello(__file__)
 
