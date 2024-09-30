87c1b0998e9d6717e6a63cf03398614dc6d7caa3
I Am Groot <groot@localhost>
2024-09-29 22:10:32 -0400

    Removed hard coded shaders

--- Python_01/ShaderLoader.py
+++ Python_01/ShaderLoader.py
@@ -6,49 +6,6 @@ from OpenGL.GL.shaders import *  #compileProgram, compileShader
 
 class ShaderLoader():
 
-    # def load_shader_hc():
-
-    #     vertex_src = """
-    #     # version 330
-
-    #     layout(location = 0) in vec3 a_position;
-    #     layout(location = 1) in vec2 a_texture;
-
-    #     uniform mat4 model; // combined translation and rotation
-    #     uniform mat4 proj;
-
-    #     out vec3 v_color;
-    #     out vec2 v_texture;
-
-    #     void main()
-    #     {
-    #         gl_Position = proj * model * vec4(a_position, 1.0);
-    #         v_texture = a_texture;
-    #     }
-    #     """
-
-    #     fragment_src = """
-    #     # version 330
-
-    #     in vec2 v_texture;
-
-    #     out vec4 out_color;
-
-    #     uniform sampler2D s_texture;
-
-    #     void main()
-    #     {
-    #         out_color = texture(s_texture, v_texture);
-    #     }
-    #     """
-    #     vert_shader = compileShader(vertex_src, GL_VERTEX_SHADER)
-    #     frag_shader = compileShader(fragment_src, GL_FRAGMENT_SHADER)    
-
-    #     shader = compileProgram(vert_shader,frag_shader)
-
-    #     print(f"Shaders loaded: {shader}")
-    #     return shader
-
     def load_shader(
             vertex_shader_filename, 
             fragment_shader_filename  ):
