cd565482dbd626bb082baae6af8d77c92478fdfa
I Am Groot <groot@localhost>
2024-10-24 12:55:10 -0400

    Move code to thread class

--- Python_01/src/learning/GUI_Test.py
+++ Python_01/src/learning/GUI_Test.py
@@ -35,53 +35,53 @@ class GuiMain():
 
         images = "./resources/imgs/"
 
-        self.root.title('Themed Widgets Example')
-        style = ttk.Style()
-        style.theme_use('clam')
-        label = ttk.Label(self.root, text="Hello, Tkinter!")
-        label.pack()
+        # self.root.title('Themed Widgets Example')
+        # style = ttk.Style()
+        # style.theme_use('clam')
+        # label = ttk.Label(self.root, text="Hello, Tkinter!")
+        # label.pack()
 
-        frame1 = tk.Frame(self.root)
-        frame1.pack()
+        # frame1 = tk.Frame(self.root)
+        # frame1.pack()
 
-        imge = Image.open(images + 'vulkanlogo.png')
-        phot = ImageTk.PhotoImage(imge)
-        button = ttk.Button(frame1, text="Click Me", image=phot)
-        button.grid(row=0,column=0)
+        # imge = Image.open(images + 'vulkanlogo.png')
+        # phot = ImageTk.PhotoImage(imge)
+        # button = ttk.Button(frame1, text="Click Me", image=phot)
+        # button.grid(row=0,column=0)
+        # # button.pack()
+
+        # self.root.title('Image Example')
+        # image = Image.open(images + 'lena.jpg')
+        # photo = ImageTk.PhotoImage(image)
+        # label = tk.Label(frame1, image=photo)
+        # label.grid(row=0,column=1)
+        # # label.pack()
+        # label = ttk.Label(self.root, text="TESTING")
+        # label.pack()
+
+        # self.root.title('Custom Styling Example')
+        # label = tk.Label(self.root, text="Fancy Label", bg="Navy", fg="white", font=("Helvetica", 26, "bold"))
+        # label.pack(pady=10, padx=10)
+        # button = tk.Button(self.root, text="Stylish Button", bg="green", fg="white", font=("Times New Roman", 12))
+        # button.pack(pady=5, padx=5)
+
+
+        # self.root.title('Grid and Frame Example')
+
+        # frame = tk.Frame(self.root)
+        # frame.pack()
+        # for i in range(3):
+        #     for j in range(5):
+        #         button = tk.Button(frame, text=f"Button {i},{j}")
+        #         button.grid(row=i, column=j)
+
+        # self.root.title('Message Box Example')
+        # def on_button_click():
+        #     messagebox.showinfo(title="Information", message="This is a Tkinter message box")
+        # button = tk.Button(self.root, text="Click Me", command=on_button_click)
         # button.pack()
 
-        self.root.title('Image Example')
-        image = Image.open(images + 'lena.jpg')
-        photo = ImageTk.PhotoImage(image)
-        label = tk.Label(frame1, image=photo)
-        label.grid(row=0,column=1)
-        # label.pack()
-        label = ttk.Label(self.root, text="TESTING")
-        label.pack()
-
-        self.root.title('Custom Styling Example')
-        label = tk.Label(self.root, text="Fancy Label", bg="Navy", fg="white", font=("Helvetica", 26, "bold"))
-        label.pack(pady=10, padx=10)
-        button = tk.Button(self.root, text="Stylish Button", bg="green", fg="white", font=("Times New Roman", 12))
-        button.pack(pady=5, padx=5)
-
-
-        self.root.title('Grid and Frame Example')
-
-        frame = tk.Frame(self.root)
-        frame.pack()
-        for i in range(3):
-            for j in range(5):
-                button = tk.Button(frame, text=f"Button {i},{j}")
-                button.grid(row=i, column=j)
-
-        self.root.title('Message Box Example')
-        def on_button_click():
-            messagebox.showinfo(title="Information", message="This is a Tkinter message box")
-        button = tk.Button(self.root, text="Click Me", command=on_button_click)
-        button.pack()
-
-        self.root.mainloop()
+        # self.root.mainloop()
 
 if __name__ == '__main__':
     wapp = GuiMain()
